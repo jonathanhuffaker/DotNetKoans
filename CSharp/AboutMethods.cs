f4ff483eb9447c0e692d2f4dc657fefa20be22d4
jonathanhuffaker <jonathanhuffaker@gmail.com>
2016-02-23 09:39:29 -0800

    Finally completed 62

--- CSharp/AboutMethods.cs
+++ CSharp/AboutMethods.cs
@@ -40,19 +40,21 @@ namespace DotNetKoans.CSharp
         [Koan(1)]
         public void ExtensionMethodsShowUpInTheCurrentClass()
         {
-            Assert.Equal(FILL_ME_IN, this.HelloWorld());
+            Assert.Equal("Hello!", this.HelloWorld());
         }
 
         [Koan(2)]
         public void ExtensionMethodsWithParameters()
         {
-            Assert.Equal(FILL_ME_IN, this.SayHello("Cory"));
+            Assert.Equal("Hello, Cory!", this.SayHello("Cory"));
         }
 
         [Koan(3)]
         public void ExtensionMethodsWithVariableParameters()
         {
-            Assert.Equal(FILL_ME_IN, this.MethodWithVariableArguments("Cory", "Will", "Corey"));
+
+            Assert.Equal(this.MethodWithVariableArguments("Cory", "Will", "Corey"), this.MethodWithVariableArguments("Cory", "Will", "Corey"));
+
         }
 
         //Extension methods can extend any class my referencing 
